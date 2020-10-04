01c46c172979667546d8b2f1fda345ab74f60b23
Cassio Tessaro <cassioate@gmail.com>
2020-10-04 18:28:30 -0300

    Service Layer, component registration

--- src/main/java/br/com/tessaro/micro/resources/UserResource.java
+++ src/main/java/br/com/tessaro/micro/resources/UserResource.java
@@ -1,20 +1,35 @@
 package br.com.tessaro.micro.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.tessaro.micro.entities.User;
+import br.com.tessaro.micro.services.UserServices;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserServices service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Cassio", "cassioate@gmail.com", "(83)9 9918-9488", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+			
 	}
 	
 }
